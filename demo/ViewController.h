410b2dc7a983888c9accdd146373dbaabfb2b2cf
derrick <derrick0714@gmail.com>
2014-03-24 23:17:03 -0400

    Initial Commit

---
+++ demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by Xu Deng on 3/24/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo
+//
+//  Created by Xu Deng on 3/24/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
