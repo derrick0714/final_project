d921d7134a53346d2b6c4a80ce52b3d34ef4f7bf
derrick <derrick0714@gmail.com>
2014-04-12 18:09:36 -0400

    add user class

---
+++ demo/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  demo
+//
+//  Created by Xu Deng on 4/12/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ demo/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  demo
+//
+//  Created by Xu Deng on 4/12/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
