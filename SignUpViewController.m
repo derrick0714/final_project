08ee202ee21975760acbd5e83fd7ba9abc3d7dc4
derrick <derrick0714@gmail.com>
2014-04-11 22:34:39 -0400

    add sign in and sign up

---
+++ SignUpViewController.m
@@ -0,0 +1,56 @@
+//
+//  SignUpViewController.m
+//  demo
+//
+//  Created by Xu Deng on 4/11/14.
+//  Copyright (c) 2014 Xu Deng. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UIColor *myColor = [[UIColor alloc]initWithRed:59.0/255.0 green:89.0/255.0 blue:152.0/255.0 alpha:1];
+    
+    self.view.backgroundColor = myColor;
+    
+ 
+}
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
