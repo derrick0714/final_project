38b9ffe6cbcd04499498f00e85a41694c46b8a4f
Luming <lumingnie@gmail.com>
2014-04-15 20:27:38 -0400

    111

--- demo/MeTableViewController.m
+++ demo/MeTableViewController.m
@@ -42,7 +42,9 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     if (!self.isApplicantToMe) {
+        acceptButton.customView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
         [acceptButton setEnabled:NO];
+        acceptButton.customView.alpha = 0.0f;
         meTableViewTitle.title = @"Applicant";
     }
     
